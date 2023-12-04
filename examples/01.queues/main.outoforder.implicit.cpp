f920ab46ebb9c157307c4d6f314029cd6ab85b3b
Saleh <saleh_jamali@ymail.com>
2023-12-04 13:49:48 +0000

    added example01 (queues).

---
+++ examples/01.queues/main.outoforder.implicit.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <CL/sycl.hpp>
+
+int main(int argc, char *argv[]) {
+    constexpr const size_t N = 1024 * 64;
+    const sycl::range VecSize{N};
+
+    sycl::buffer<double> bufA{VecSize};
+    sycl::buffer<double> bufB{VecSize};
+    sycl::buffer<double> bufC{VecSize};
+
+    // Initialize input data
+    {
+        sycl::host_accessor h_a{bufA, sycl::write_only};
+        sycl::host_accessor h_b{bufB, sycl::write_only};
+
+        for (int i = 0; i < N; i++) {
+            h_a[i] = sin(i);
+            h_b[i] = cos(i);
+        }
+    }
+
+    sycl::device d = sycl::device(sycl::default_selector_v);
+
+    std::cout << "Device: " << std::endl;
+    std::cout << "\tName: " << d.get_info<sycl::info::device::name>() << std::endl;
+    std::cout << "\tMax Compute Units: " << d.get_info<sycl::info::device::max_compute_units>() << std::endl;
+
+    sycl::queue q{d}; // out-of-order queue
+
+    auto event1 = q.submit([&](sycl::handler &h) {
+        auto a = bufA.get_access<sycl::access::mode::read_write>(h);
+        h.parallel_for(VecSize, [=](sycl::id<1> i) {
+            a[i] = a[i] * a[i];
+        });
+    });
+
+    auto event2 = q.submit([&](sycl::handler &h) {
+        auto b = bufB.get_access<sycl::access::mode::read_write>(h);
+        h.parallel_for(VecSize, [=](sycl::id<1> i) {
+            b[i] = b[i] * b[i];
+        });
+    });
+
+    auto event3 = q.submit([&](sycl::handler &h) {
+        const auto read_t = sycl::access::mode::read;
+        const auto write_t = sycl::access::mode::write;
+
+        auto a = bufA.get_access<read_t>(h);
+        auto b = bufB.get_access<read_t>(h);
+        auto c = bufC.get_access<write_t>(h);
+
+        h.parallel_for(VecSize, [=](sycl::id<1> i) {
+            c[i] = a[i] + b[i];
+        });
+    });
+
+    {
+        sycl::host_accessor h_c{bufC, sycl::read_only};
+
+        double sum = 0.0f;
+        for (int i = 0; i < N; i++) {
+            sum += h_c[i];
+        }
+        std::cout << "Sum is : " << sum << std::endl;
+    }
+
+    return 0;
+}
---
+++ examples/01.queues/readme.md
@@ -0,0 +1,38 @@
+# Example 01: Queues
+
+## Queues
+
+```mermaid
+graph TD;
+  Queues-->In-order;
+  Queues-->Out-of-order;
+  Out-of-order-->Implicit Ordering;
+  Out-of-order-->Explicit Ordering;
+```
+
+### In-order
+
+Queues are in nature out-of-order. In-order queues could be instantiated
+with `sycl::queue q{my_device, sycl::property::queue::in_order()};`.
+
+### Out-of-order
+
+The out-of-order queues could infer ordering from **implicit** or **explicit** ordering. Implicit ordering is done using
+Command
+Groups.
+
+#### Explicit Ordering
+
+Is done using events, similar to that of CUDA and OpenCL. Each CG upon submission to a queue gets an event (returned
+from `sycl::queue::submit()`).
+This event could be used to define dependencies for latter CGs. Also, a blocking `wait()` could be called on an event to
+make sure its CG has finished before proceeding on the host thread.
+The dependencies could be defined with `handler.depends_on(event1);` within a CG. These could be used to construct
+Computational Graph or Directed Acyclic Graph (DAG), just like non-eager execution in Tensorflow. If you are familiar
+with CUDA or OpenCL these concepts should not feel strange.
+
+#### Implicit Ordering
+
+Is done simply by using `sycl::accessor`s for buffers and the programmer is not required to do anything extra.
+Just use `my_buffer.get_access<access_t>(handler);` in CG with `const auto access_t = sycl::access::mode::write` or any
+other element of that enum.
